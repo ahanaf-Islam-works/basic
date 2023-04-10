5050749da85d0516430acda73ee68cc901eed16f
Ahanaf Islam <122388246+ahanaf-Islam-works@users.noreply.github.com>
2023-04-10 16:22:00 +0600

    Add files via upload

---
+++ dataContext.js
@@ -0,0 +1,24 @@
+"use client";
+import { createContext, useContext, useState } from "react";
+
+const initialData = [
+  { id: 1, name: "John" },
+  { id: 2, name: "Jane" },
+];
+
+export const DataContext = createContext({
+  data: initialData,
+  setData: () => {},
+});
+
+export const DataContextProvider = ({ children }) => {
+  const [data, setData] = useState(initialData);
+
+  return (
+    <DataContext.Provider value={{data, setData} }>
+      {children}
+    </DataContext.Provider>
+  );
+};
+
+export const useDataContext = () => useContext(DataContext);
